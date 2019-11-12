091d7fe725da51ccfae918f2cf4b2bd8e6133b49
jdavidpm <94.david.pm@gmail.com>
2019-11-11 21:23:35 -0600

    Every change

---
+++ js/init.js
@@ -0,0 +1,22 @@
+(function($){
+  $(function(){
+    $('.datepicker').datepicker();
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $('.materialboxed').materialbox();
+    $('.slider').slider({
+      indicators: false,
+      height: 600,
+      duration: 500,
+      interval: 2000
+    });
+    $('.tooltipped').tooltip();
+    $('.dropdown-trigger').dropdown({
+        constrainWidth: false,
+        coverTrigger: false,
+        closeOnClick: true,
+        alignment: 'right'
+      }
+    );
+  });
+})(jQuery);
