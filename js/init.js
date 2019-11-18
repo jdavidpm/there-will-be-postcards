97865b7ab949dd8335b58643a1e9014b3f18ec22
jdavidpm <94.david.pm@gmail.com>
2019-11-18 14:29:40 -0600

    search bar

--- js/init.js
+++ js/init.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
+  $('input.autocomplete').autocomplete({
+    data: {
+      "Apple": null,
+      "Microsoft": null,
+      "Google": 'https://placehold.it/250x250'
+    },
+  });
   $('select').formSelect();
   $('.carousel').carousel();
   $('.datepicker').datepicker();
