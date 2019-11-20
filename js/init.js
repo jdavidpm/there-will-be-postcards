12f6954a49572d83ef604261b8e5fe1e0c5fe89a
jdavidpm <94.david.pm@gmail.com>
2019-11-19 21:52:45 -0600

    search bar and send view

--- js/init.js
+++ js/init.js
@@ -26,4 +26,30 @@ $( document ).ready(function() {
       alignment: 'right'
     }
   );
+
+  $('.chips').chips();
+  $('.chips-initial').chips({
+    data: [{
+      tag: 'Apple',
+    }, {
+      tag: 'Microsoft',
+    }, {
+      tag: 'Google',
+    }],
+  });
+  $('.chips-placeholder').chips({
+    placeholder: 'Enter a tag',
+    secondaryPlaceholder: '+Tag',
+  });
+  $('.chips-autocomplete').chips({
+    autocompleteOptions: {
+      data: {
+        'Apple': null,
+        'Microsoft': null,
+        'Google': null
+      },
+      limit: Infinity,
+      minLength: 1
+    }
+  });
 });
